e53a59ced8795105c44a7ef0528b2f525fe8d91b
Lewsha24 <sadkovoi0@gmial.com>
2023-04-06 15:14:10 +0700

    updated url

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+//VALUE
 const data = {
-     'client_id': '1',
-     'client_secret': '1'
+     'client_id': 'EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI',
+     'client_secret': 'PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG',
 }
 
 async function getAPIAnswerFromCdek(){
      try {
-          let response = await fetch('https://api.edu.cdek.ru/v2/oauth/token?parameters', {
+          let response = await fetch('https://api.edu.cdek.ru/v2/oauth/token?grant_type=client_credentials&client_id=EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI&client_secret=PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG', {
                method:'POST',
                headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                },
-               body: JSON.stringify(data),
+//               body: JSON.stringify(data),
                mode:'no-cors',
-               cache: 'no-cache',
+               // cache: 'no-cache',
           })
-          return response.json();         
-          
+//          console.log(response)
+          return response.json()
+
      }    catch(err) {
           return err;
      }
