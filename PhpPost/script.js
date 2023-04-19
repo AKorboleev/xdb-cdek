3de5543f94823c7b09f01a8e21eb87a16045f4d2
Lewsha24 <sadkovoi0@gmial.com>
2023-04-19 15:34:33 +0700

    updated

--- PhpPost/script.js
+++ PhpPost/script.js
@@ -1,4 +1,49 @@
 //-------------------------------------------------------------------
+// Отправляем в handler с данными доставки , работает от onclick()
+async function postSdek() {
+
+    // Название тарифа
+    let tariffName = document.querySelector('h3.tariff_name');
+    // Минимальная доставка
+    let calendarMin = document.querySelector('.calendar__min');
+    // Максимальная доставка
+    let calendarMax = document.querySelector('.calendar__max');
+    // Сумма доставки
+    let deliverySum = document.querySelector('.delivery_sum');
+
+    // Массив с данными
+    let delivery = {
+        'tariffName': tariffName.textContent,
+        'calendarMin': calendarMin.textContent,
+        'calendarMax': calendarMax.textContent,
+        'deliverySum': deliverySum.textContent,
+    };
+
+    // Отправка данных выбранной доставки
+    try {
+        let response = await fetch('http://xdb-cdek/PhpPost/sdek2.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'no-cors',
+            cache: 'no-cache',
+            body: JSON.stringify(delivery)
+        });
+        // Проверка ушел ли запрос в sdek2.php
+        if (!response.ok) {
+            // Ошибка с получаемыми данными
+            console.log('Ошибка HTTP: ' + response.status);
+        }
+        // если HTTP-статус в диапазоне 200-299
+        else {
+            console.log('запрос ушел');
+        }
+    } catch (error) {
+        return error;
+    }
+}
+//-------------------------------------------------------------------
 // функция для запроса в handler.php за json файлом и вывод данных на страницу
 async function postJson() {
     try {
@@ -59,50 +104,3 @@ async function postJson() {
 
 // Отправляем запрос в handler
 postJson();
-
-//-------------------------------------------------------------------
-// Отправляем в handler с данными доставки
-async function postSdek() {
-
-    // Название тарифа
-    let tariffName = document.querySelector('h3.tariff_name');
-    // Минимальная доставка
-    let calendarMin = document.querySelector('.calendar__min');
-    // Максимальная доставка
-    let calendarMax = document.querySelector('.calendar__max');
-    // Сумма доставки
-    let deliverySum = document.querySelector('.delivery_sum');
-
-    // Массив с данными
-    let delivery = {
-        'tariffName': tariffName.textContent,
-        'calendarMin': calendarMin.textContent,
-        'calendarMax': calendarMax.textContent,
-        'deliverySum': deliverySum.textContent,
-    };
-
-    // Отправка данных выбранной доставки
-    try {
-        let response = await fetch('http://xdb-cdek/PhpPost/sdek2.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            mode: 'no-cors',
-            cache: 'no-cache',
-            body: JSON.stringify(delivery)
-        });
-        // Проверка ушел ли запрос в sdek2.php
-        if (!response.ok) {
-            // Ошибка с получаемыми данными
-            console.log('Ошибка HTTP: ' + response.status);
-        }
-        // если HTTP-статус в диапазоне 200-299
-        else {
-            console.log('запрос ушел');
-        }
-    } catch (error) {
-        return error;
-    }
-}
-//-------------------------------------------------------------------
