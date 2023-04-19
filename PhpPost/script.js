e9638cb498df2c1cc4675d68e3a5ec818d2bf2e6
Lewsha24 <sadkovoi0@gmial.com>
2023-04-19 15:23:10 +0700

    updated

--- PhpPost/script.js
+++ PhpPost/script.js
@@ -2,60 +2,59 @@
 // функция для запроса в handler.php за json файлом и вывод данных на страницу
 async function postJson() {
     try {
-            let response = await fetch('http://xdb-cdek/PhpPost/handler.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/application/json',
-                },
-                mode: 'no-cors',
-                cache: 'no-cache',
-            });
-            // Проверка прошел ли запрос в handler.php
-            if (!response.ok) {
-                // Ошибка с получаемыми данными
-                console.log('Ошибка HTTP: ' + response.status);
-            } else { // если HTTP-статус в диапазоне 200-299
-                // Читаем ответ в формате JSON
-                let content = await response.json();
+        let response = await fetch('http://xdb-cdek/PhpPost/handler.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/application/json',
+            },
+            mode: 'no-cors',
+            cache: 'no-cache',
+        });
+        // Проверка прошел ли запрос в handler.php
+        if (!response.ok) {
+            // Ошибка с получаемыми данными
+            console.log('Ошибка HTTP: ' + response.status);
+        } else { // если HTTP-статус в диапазоне 200-299
+            // Читаем ответ в формате JSON
+            let content = await response.json();
+            // Контейнер где хронятся все
+            let containerBox = document.querySelector(".container__box");
 
-                // Контейнер где хронятся все
-                let containerBox = document.querySelector(".container__box");
+            containerBox.style.display = 'block';
 
-                containerBox.style.display = 'block';
+            // Название тарифа
+            let tariffName = document.querySelector("h3.tariff_name");
 
-                // Название тарифа
-                let tariffName = document.querySelector("h3.tariff_name");
+            // Минимальная доставка
+            let calendarMin = document.querySelector(".calendar__min");
 
-                // Минимальная доставка
-                let calendarMin = document.querySelector(".calendar__min");
+            // Максимальная доставка
+            let calendarMax = document.querySelector(".calendar__max");
 
-                // Максимальная доставка
-                let calendarMax = document.querySelector(".calendar__max");
+            // Сумма доставки
+            let deliverySum = document.querySelector(".delivery_sum");
 
-                // Сумма доставки
-                let deliverySum = document.querySelector(".delivery_sum");
+            // Цикл для вывода контента
+            for (let k in content.tariff_codes) {
+                //-------------------------------------------------------------------
+                // Копируем контейнер с данными и выводим его
+                let containerClone = containerBox.cloneNode(true)
+                containerClone.style.display = 'block'
+                document.body.appendChild(containerClone)
+                //-------------------------------------------------------------------
 
-                // Цикл для вывода контента
-                for (let k in content.tariff_codes) {
-                    //-------------------------------------------------------------------
-                    // Копируем контейнер с данными и выводим его
-                    let containerClone = containerBox.cloneNode(true)
-                    containerClone.style.display = 'block'
-                    document.body.appendChild(containerClone)
-                    //-------------------------------------------------------------------
-
-                    //-------------------------------------------------------------------
-                    // Данные которые требуется вывети
-                    tariffName.textContent = content.tariff_codes[k].tariff_name;
-                    calendarMax.textContent = content.tariff_codes[k].calendar_max;
-                    calendarMin.textContent = content.tariff_codes[k].calendar_min;
-                    deliverySum.textContent = content.tariff_codes[k].delivery_sum;
-                    //-------------------------------------------------------------------
-                }
+                //-------------------------------------------------------------------
+                // Данные которые требуется вывети
+                tariffName.textContent = content.tariff_codes[k].tariff_name;
+                calendarMax.textContent = content.tariff_codes[k].calendar_max;
+                calendarMin.textContent = content.tariff_codes[k].calendar_min;
+                deliverySum.textContent = content.tariff_codes[k].delivery_sum;
+                //-------------------------------------------------------------------
             }
-        } catch (error) {
-            return  error;
         }
+    } catch (error) {
+        return  error;
+    }
 }
 //-------------------------------------------------------------------
 
--- PhpPost/sdek2.php
+++ PhpPost/sdek2.php
@@ -29,85 +29,86 @@ use Symfony\Component\HttpClient\Psr18Client;
     // Максимальное время доставки
     $calendarMax = $json->calendarMax;
 
-// Вход в аккаунт
-//-------------------------------------------------------------------
-try {
-    $cdek->authorize();
-    $cdek->getToken();
-    $cdek->getExpire();
-    print_r('123123123');
-} catch (AuthException $exception) {
-    //Авторизация не выполнена, не верные account и secure
-    echo $exception->getMessage();
-    print_r('error auth');
-}
+
+    //-------------------------------------------------------------------
+    // Вход в аккаунт
+    try {
+        $cdek->authorize();
+        $cdek->getToken();
+        $cdek->getExpire();
+        print_r('123123123');
+    } catch (AuthException $exception) {
+        //Авторизация не выполнена, не верные account и secure
+        echo $exception->getMessage();
+        print_r('error auth');
+    }
 
 
 
-// Создание заказа
-//-------------------------------------------------------------------
-$order = BaseTypes\Order::create([
-    'number' => '3627',
-    'tariff_code' => '1',
-    'sender' => BaseTypes\Contact::create([
-        'name' => 'Vasya',
-    ]),
-    'recipient' => BaseTypes\Contact::create([
-        'name' => 'Alexander',
-        'phones' => [
-            BaseTypes\Phone::create(['number' => '88001234567'])
-        ]
-    ]),
-    'from_location' => BaseTypes\Location::create([
-        'code' => 137,
-        'country_code' => 'ru'
-    ]),
-    'to_location' => BaseTypes\Location::create([
-        'code' => 270,
-        'country_code' => 'ru',
-        'address' => 'Титова 21 кв 9'
-    ]),
-    'packages' => [
-        BaseTypes\Package::create([
-            'number' => 'number1',
-            'weight' => 1000,
-            'length' => 12,
-            'width' => 11,
-            'height' => 8,
-            'items' => [
-                BaseTypes\Item::create([
-                    'name' => 'Toys for man',
-                    'ware_key' => 'Items_number_5',
-                    'payment' => BaseTypes\Money::create(['value' => 0]),
-                    'cost' => 0,
-                    'weight' => 1000,
-                    'amount' => 1,
-                ]),
+    // Создание заказа
+    //-------------------------------------------------------------------
+    $order = BaseTypes\Order::create([
+        'number' => '3627',
+        'tariff_code' => '1',
+        'sender' => BaseTypes\Contact::create([
+            'name' => 'Vasya',
+        ]),
+        'recipient' => BaseTypes\Contact::create([
+            'name' => 'Alexander',
+            'phones' => [
+                BaseTypes\Phone::create(['number' => '88001234567'])
             ]
-        ])
-    ],
-]);
-//-------------------------------------------------------------------
+        ]),
+        'from_location' => BaseTypes\Location::create([
+            'code' => 137,
+            'country_code' => 'ru'
+        ]),
+        'to_location' => BaseTypes\Location::create([
+            'code' => 270,
+            'country_code' => 'ru',
+            'address' => 'Титова 21 кв 9'
+        ]),
+        'packages' => [
+            BaseTypes\Package::create([
+                'number' => 'number1',
+                'weight' => 1000,
+                'length' => 12,
+                'width' => 11,
+                'height' => 8,
+                'items' => [
+                    BaseTypes\Item::create([
+                        'name' => 'Toys for man',
+                        'ware_key' => 'Items_number_5',
+                        'payment' => BaseTypes\Money::create(['value' => 0]),
+                        'cost' => 0,
+                        'weight' => 1000,
+                        'amount' => 1,
+                    ]),
+                ]
+            ])
+        ],
+    ]);
+    //-------------------------------------------------------------------
 
 
 
-// Отправка данных
-//-------------------------------------------------------------------
-try {
-    $result = $cdek->orders()->add($order);
-    if ($result->isOk()) {
-        //Запрос успешно выполнился
-        $response_order = $cdek->formatResponse($result, BaseTypes\Order::class);
-        // получаем UUID заказа и сохраняем его
-        $response_order->entity->uuid;
+    // Отправка данных
+    //-------------------------------------------------------------------
+    try {
+        $result = $cdek->orders()->add($order);
+        if ($result->isOk()) {
+            //Запрос успешно выполнился
+            $response_order = $cdek->formatResponse($result, BaseTypes\Order::class);
+            // получаем UUID заказа и сохраняем его
+            $response_order->entity->uuid;
+        }
+        if ($result->hasErrors()) {
+            // Обрабатываем ошибки
+
+        }
+    } catch (RequestException $exception) {
+        echo $exception->getMessage();
     }
-    if ($result->hasErrors()) {
-        // Обрабатываем ошибки
-
-    }
-} catch (RequestException $exception) {
-    echo $exception->getMessage();
-}
-//-------------------------------------------------------------------
+    //-------------------------------------------------------------------
 
 
